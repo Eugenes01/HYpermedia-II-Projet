b3fdcf8031d338a23b5836d3b08963ba32f56b8d
Eugenes01 <eugenes01workshop@gmail.com>
2017-10-19 13:36:35 -0400

    Update config.php

--- Aeroport Projet/model/config/config.php
+++ Aeroport Projet/model/config/config.php
@@ -1,1 +1,9 @@
-
+<?php	
+	interface Config
+	{	
+		const URL = "localhost/Projet Aeroport";
+		const DB_HOST = "localhost";
+		const DB_USER = "root";
+		const DB_PWD = "root";
+		const DB_NAME = "aeroportdb";
+	}
